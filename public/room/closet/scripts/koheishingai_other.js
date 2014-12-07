2d5a9177bf7e0594f6e470ff77dcd1ce171b37f7
koheishingai <koheishingai@gmail.com>
2014-12-08 03:00:02 +0900

    test7

--- public/room/closet/scripts/koheishingai_other.js
+++ public/room/closet/scripts/koheishingai_other.js
@@ -1,6 +1,31 @@
-this.create = this.create || {};
-(function(create) {
+this.koheishingai = this.koheishingai || {};
+(function(koheishingai) {
     'Use Strict'
+    
+    koheishingai.loading_start = "<div class='partial-load object-n";
+    koheishingai.loading_end = "' style='width:100%;height:100%;'><img src='./images/load.svg'></div>";
+    
+    koheishingai.getObject = function() {
+        var go = {};
+        go.cnt = 0;
+        $('[object]').each(function() {
+            $(this).html(koheishingai.loading_start+go.cnt+koheishingai.loading_end);
+            $(this).attr("object-id", go.cnt);
+            go.args = $(this).attr("args");
+            if (room.getLang() === "ja") {
+                koheishingai.ajax("getObject", $(this).attr("object") + "_ja", go.cnt, go.args);
+            } else {
+                koheishingai.ajax("getObject", $(this).attr("object") + "_en", go.cnt, go.args);
+            }
+            go.cnt++;
+        });
+    };
+    
+    koheishingai.init = function(){
+      koheishingai.getObject();
+    }
+    
+    koheishingai.init();
 
-}(this.create));
+}(this.koheishingai));
 //Kohei Shingai
