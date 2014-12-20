22113a6a2cc5cee0af6786e43a0fd7380f17b9f2
koheishingai <koheishingai@gmail.com>
2014-12-20 22:31:54 +0900

    fix 20-11

--- public/room/closet/scripts/article-panel-type01.js
+++ public/room/closet/scripts/article-panel-type01.js
@@ -146,10 +146,10 @@
             object.setPanel();
             object.loadData(object.article_name);
             if($('body').hasClass("mobile") === true){
-              $('.thumb h1').hide();
+              $('.thumb').hide();
               setTimeout(function() {
                 object.loadData(object.article_name);
-                $('.thumb h1').fadeIn();
+                $('.thumb').fadeIn();
               }, 1350);
             }
         }, 1350);
