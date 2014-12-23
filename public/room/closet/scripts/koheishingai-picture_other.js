ceca29cd70d4015e5dc231d0dbd1a74d2fc5be57
koheishingai <koheishingai@gmail.com>
2014-12-23 20:48:03 +0900

    22-07

--- public/room/closet/scripts/koheishingai-picture_other.js
+++ public/room/closet/scripts/koheishingai-picture_other.js
@@ -35,6 +35,8 @@ this.picture = this.picture || {};
           gp.outWidth = 0 - (parseInt(picture.$picture_out.css("width"))/2 - 130);
         }
         setTimeout(function(){
+          console.log(gp.outHeight);
+          console.log(gp.outWidth);
           picture.$picture_out.css("margin-top", gp.outHeight);
           picture.$picture_out.css("margin-left", gp.outWidth);
           picture.$picture_out.fadeIn().css("display", "block");
